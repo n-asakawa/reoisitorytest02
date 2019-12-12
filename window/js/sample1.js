e5f00ded75d9654049e9640b361ecbbca8ddb426
n-asakawa <janomerica@gmail.com>
2019-12-12 10:25:39 +0900

    new window

--- window/js/sample1.js
+++ window/js/sample1.js
@@ -1,2 +1,25 @@
 'use strict';
 
+$(function(){
+  $("a.open").click(function(){
+    $("#floatWindow").fadeIn("fast");
+    return false;
+  });
+
+  $("#floatWindow a.close").click(function(){
+    $("#floatWindow").fadeOut("fast");
+    return false;
+  });
+
+  $("#floatWindow dl dt").mousedown(function(e){
+    $("#floatWindow").data("clickPointX", e.pageX - $("#floatWindow").offset().left).data("clickPointY", e.pageY - $("#floatWindow").offset().top);
+    $(document).mousemove(function(e){
+      $("#floatWindow").css({
+        "top": e.pageY - $("#floatWindow").data("clickPointY") + "px",
+        "left": e.pageX - $("#floatWindow").data("clickPointX") + "px"
+      });
+    });
+  }).mouseup(function(){
+    $(document).off("mousemove");
+  });
+});
